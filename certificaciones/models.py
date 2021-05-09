cc5fae6efcec590fe9ce1104b0c855d74b87c9b8
RojasxAndres <afrojaslondono@gmail.com>
2021-05-09 10:56:46 -0500

    Modificacando certificaciones

--- certificaciones/models.py
+++ certificaciones/models.py
@@ -3,12 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Certificaciones(models.Model):
-    curso          = models.CharField(max_length=50)
+    nombre         = models.CharField(max_length=50)
     url            = models.URLField(max_length = 200)
     contenido      = models.TextField()
     imagen         = models.ImageField(upload_to="certificaciones",null=True,blank=True)
     created        = models.DateTimeField(auto_now_add=True)
     updated        = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.nombre
     class Meta:
         verbose_name = "Certificacion"
         verbose_name_plural="Certificaciones"
+    
