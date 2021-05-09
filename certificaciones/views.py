dfb2950dba8d1447a745a5e0bf10c04d924a21d8
RojasxAndres <afrojaslondono@gmail.com>
2021-05-09 11:10:53 -0500

    Añadiendo cards a certificaciones

--- certificaciones/views.py
+++ certificaciones/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Certificaciones
 # Create your views here.
 def certificaciones(req):
-    return render(req,"certificaciones/certificaciones.html")
+    certificaciones = Certificaciones.objects.all()
+    return render(req,"certificaciones/certificaciones.html",{"certificaciones":certificaciones})
